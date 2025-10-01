895f09475f569186bd407a37ad4599e36c25e055
ringyuki <hikarinagi@outlook.com>
2025-10-01 11:12:11 +0800

    feat(upload): add new compressed file type support

--- enums/upload/allowed-large-file.enum.ts
+++ enums/upload/allowed-large-file.enum.ts
@@ -4,6 +4,7 @@ export enum AllowedLargeArchiveFileMimeTypes {
   'application/x-rar' = 'application/x-rar',
   'application/vnd.rar' = 'application/vnd.rar',
   'application/x-rar-compressed' = 'application/x-rar-compressed',
+  'application/x-compressed' = 'application/x-compressed',
   'application/x-7z-compressed' = 'application/x-7z-compressed',
   'application/x-tar' = 'application/x-tar',
   'application/x-gzip' = 'application/x-gzip',
