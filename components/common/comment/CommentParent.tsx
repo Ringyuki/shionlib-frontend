db6e1431a32f92e7941d9090bc03cab8787562e8
ringyuki <hikarinagi@outlook.com>
2025-10-10 10:05:31 +0800

    fix(comment): fix types error

--- components/common/comment/CommentParent.tsx
+++ components/common/comment/CommentParent.tsx
@@ -10,7 +10,7 @@ interface CommentParentProps {
 export const CommentParent = ({ parent }: CommentParentProps) => {
   const t = useTranslations('Components.Common.Comment.CommentParent')
   const handleClick = () => {
-    const commentContent = document.getElementById(`data-comment-id-${parent.id}`)
+    const commentContent = document.getElementById(`data-comment-id-${parent?.id}`)
     if (commentContent) {
       commentContent.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' })
       commentContent.classList.add('bg-card-hover')
@@ -21,20 +21,22 @@ export const CommentParent = ({ parent }: CommentParentProps) => {
   }
 
   return (
-    <div
-      className="flex flex-col gap-2 rounded-md border p-4 hover:bg-card-hover cursor-pointer transition-colors duration-200"
-      onClick={handleClick}
-    >
-      <div className="flex items-center gap-2">
-        <span className="text-sm font-medium">{t('parent')}</span>
-        <Avatar user={parent.creator} className="size-6 text-xs" />
-        <div className="flex flex-col">
-          <span className="text-sm font-medium">{parent.creator.name}</span>
+    parent && (
+      <div
+        className="flex flex-col gap-2 rounded-md border p-4 hover:bg-card-hover cursor-pointer transition-colors duration-200"
+        onClick={handleClick}
+      >
+        <div className="flex items-center gap-2">
+          <span className="text-sm font-medium">{t('parent')}</span>
+          <Avatar user={parent.creator} className="size-6 text-xs" />
+          <div className="flex flex-col">
+            <span className="text-sm font-medium">{parent.creator.name}</span>
+          </div>
         </div>
+        <ScrollArea className="h-12 max-h-24">
+          <div dangerouslySetInnerHTML={{ __html: parent.html || '' }} />
+        </ScrollArea>
       </div>
-      <ScrollArea className="h-12 max-h-24">
-        <div dangerouslySetInnerHTML={{ __html: parent.html || '' }} />
-      </ScrollArea>
-    </div>
+    )
   )
 }
