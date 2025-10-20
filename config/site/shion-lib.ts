6e42b7ac3dd4cebde6deddc7325cf552dd6936ad
ringyuki <hikarinagi@outlook.com>
2025-10-20 17:44:57 +0800

    fix(config): fix type error

--- config/site/shion-lib.ts
+++ config/site/shion-lib.ts
@@ -1,17 +1,9 @@
 import { ShionlibSiteConfig } from '@/interfaces/site/shion-lib-site-config.interface'
 
 export const shionlibSiteConfig: ShionlibSiteConfig = {
-  title: '书音的图书馆 - Galgame 资源站！',
-  titleShort: '书音的图书馆',
-  template: '%s - 书音的图书馆',
   canonical: 'https://shionlib.com',
-  og: {
-    title: '书音的图书馆 - Galgame 资源站！',
-    description: '书音的图书馆 - 免费、开源、零门槛的 Galgame 资源站',
-    image: 'https://shionlib.com/logo.png',
-    url: 'https://shionlib.com',
+  robots: {
+    index: true,
+    follow: true,
   },
-  description: '书音的图书馆 - 免费、开源、零门槛的 Galgame 资源站',
-  keywords: ['书音的图书馆', 'Galgame', '资源站', '免费', '开源', '零门槛'],
-  image: 'https://shionlib.com/logo.png',
 }
