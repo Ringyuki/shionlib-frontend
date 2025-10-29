465f3d3ba3d822a14c68c90aaa6609ac9d5f8e9b
ringyuki <hikarinagi@outlook.com>
2025-10-29 21:51:37 +0800

    fix(og): improve URL validation logic in Open Graph image route to handle invalid requests more gracefully

--- app/og/to-png/route.ts
+++ app/og/to-png/route.ts
@@ -5,12 +5,11 @@ export const runtime = 'nodejs'
 
 export async function GET(req: Request) {
   let url = new URL(req.url).searchParams.get('u')
-
-  if (!url || !allowedHosts.includes(new URL(url).hostname))
-    return new Response('Invalid image URL', { status: 400 })
-
+  if (!url) return new Response('Invalid request', { status: 400 })
   if (!url?.startsWith('https'))
     url = new URL(url, process.env.NEXT_PUBLIC_SHIONLIB_IMAGE_BED_URL!).toString()
+  if (!url || !allowedHosts.includes(new URL(url).hostname))
+    return new Response('Invalid image URL', { status: 400 })
 
   const webp = await fetch(url).then(r => r.arrayBuffer())
   const pngBuf = await sharp(Buffer.from(webp)).png().toBuffer()
