c0ae5c7ffd676e9d61caf0532f37120c56966f58
ringyuki <hikarinagi@outlook.com>
2025-11-23 20:47:37 +0800

    fix(friend-links): update Nysoure url and logo

--- config/friend-links/links.ts
+++ config/friend-links/links.ts
@@ -20,8 +20,8 @@ export const friendLinks: FriendLink[] = [
   {
     id: 3,
     name: 'Nysoure',
-    url: 'https://res.nyne.dev/',
-    logo: 'https://res.nyne.dev/icon-192.png',
+    url: 'https://nysoure.com/',
+    logo: 'https://nysoure.com/icon-192.png',
     description: 'Nysoure 是一个完全免费的galgame分享网站, 不接受捐赠和广告',
   },
   {
