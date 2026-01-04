9f966c11f228796d5ea747a8f19566e144ec2e20
clown145 <kisaki520@qq.com>
2026-01-04 18:08:31 +0800

    feat: improve aria2 guidance and login requirement handling

--- app/[locale]/user/settings/personal/page.tsx
+++ app/[locale]/user/settings/personal/page.tsx
@@ -1,12 +1,12 @@
 import { shionlibRequest } from '@/utils/shionlib-request'
 import { User } from '@/interfaces/user/user.interface'
 import { UserSettings } from '@/components/user/settings/UserSettings'
-import { notFound } from 'next/navigation'
+import { LoginRequired } from '@/components/user/settings/LoginRequired'
 
 export default async function UserPersonalSettingsPage() {
   const data = await shionlibRequest().get<User>('/user/me')
   if (!data.data) {
-    notFound()
+    return <LoginRequired />
   }
   return <UserSettings user={data.data} />
 }
--- app/[locale]/user/settings/site/page.tsx
+++ app/[locale]/user/settings/site/page.tsx
@@ -1,12 +1,12 @@
 import { shionlibRequest } from '@/utils/shionlib-request'
 import { User } from '@/interfaces/user/user.interface'
 import { SiteSettings } from '@/components/user/settings/SiteSettings'
-import { notFound } from 'next/navigation'
+import { LoginRequired } from '@/components/user/settings/LoginRequired'
 
 export default async function UserSiteSettingsPage() {
   const data = await shionlibRequest().get<User>('/user/me')
   if (!data.data) {
-    notFound()
+    return <LoginRequired />
   }
   return <SiteSettings user={data.data} />
 }
