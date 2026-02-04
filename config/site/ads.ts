853f015c1293826047df9e84b3d6ce517aa22480
ringyuki <hikarinagi@outlook.com>
2026-02-04 19:29:38 +0800

    chore(ads): update image url for ad 1

--- config/site/ads.ts
+++ config/site/ads.ts
@@ -4,7 +4,7 @@ import { supportedLocalesEnum } from '@/config/i18n/supported'
 export const ads: Ad[] = [
   {
     id: 1,
-    image_zh: 'https://images.yurari.moe/admin/6740325e87e2312f66fa3f58/w3hmpqgv_2.webp',
+    image_zh: 'https://images.yurari.moe/admin/6740325e87e2312f66fa3f58/t5xs943z_2.webp',
     aspect: '6.4 / 1',
     link: 'https://s.himoe.uk/ycnbdn',
     excludeLocales: [supportedLocalesEnum.EN, supportedLocalesEnum.JP],
